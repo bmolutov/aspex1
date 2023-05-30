a320348002520bd8c33627da7b24e5f025b68ecb
bmolutov <bekzatmolutov@gmail.com>
2023-05-30 21:49:23 +0600

    db models are added, ERD is modified

---
+++ app/db/models/booking.py
@@ -0,0 +1,34 @@
+from sqlalchemy import Boolean, Column, ForeignKey, Integer, DateTime, Table
+from sqlalchemy.orm import relationship
+from datetime import time
+
+from app.db.database import Base
+
+booking_table = Table('booking_table', Base.metadata,
+                      Column('booking_id', ForeignKey('bookings.id'), primary_key=True),
+                      Column('table_id', ForeignKey('tables.id'), primary_key=True)
+                      )
+
+
+class Booking(Base):
+    __tablename__ = "bookings"
+
+    id = Column(Integer, primary_key=True, index=True)
+    booking_time_start = Column(DateTime, default=time(14, 0))
+    booking_time_end = Column(DateTime, default=time(17, 0))
+    customer_id = Column(Integer, ForeignKey("customers.id"))
+
+    customer = relationship("Customer", back_populates="bookings")
+    tables = relationship("Table", secondary="booking_table", back_populates="bookings")
+
+
+class Table(Base):
+    __tablename__ = "tables"
+
+    id = Column(Integer, primary_key=True, index=True)
+    capacity = Column(Integer, default=2)
+    available_time_start = Column(DateTime, default=time(12, 0))
+    available_time_end = Column(DateTime, default=time(22, 0))
+    is_booked = Column(Boolean, default=False)
+
+    bookings = relationship("Booking", secondary="booking_table", back_populates="tables")
---
+++ app/db/models/customer.py
@@ -0,0 +1,17 @@
+from sqlalchemy import Boolean, Column, Integer, String
+from sqlalchemy.orm import relationship
+
+from app.db.database import Base
+
+
+class Customer(Base):
+    __tablename__ = "customers"
+
+    id = Column(Integer, primary_key=True, index=True)
+    email = Column(String, unique=True, index=True)
+    name = Column(String)
+    phone = Column(String, unique=True)
+    hashed_password = Column(String)
+    is_active = Column(Boolean, default=True)
+
+    bookings = relationship("Bookings", back_populates="customer")
